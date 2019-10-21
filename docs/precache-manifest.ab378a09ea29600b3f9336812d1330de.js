09e4cf3c3e679518b838efad8b7d2d0042447d98
taleore <taleore@gmail.com>
2019-10-21 12:34:30 +0200

    build docs

---
+++ docs/precache-manifest.ab378a09ea29600b3f9336812d1330de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57bde47402b6623bb56de01c308bd27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27b84a4e981d6be01859",
+    "url": "./static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "042a66e7697f24bc3b5b",
+    "url": "./static/js/2.9b0a9925.chunk.js"
+  },
+  {
+    "revision": "27b84a4e981d6be01859",
+    "url": "./static/js/main.72f4193d.chunk.js"
+  },
+  {
+    "revision": "b12ab463f688cb93af32",
+    "url": "./static/js/runtime~main.644e6f99.js"
+  }
+]);
